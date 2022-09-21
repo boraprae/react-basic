af38f8a0ab27b0740f44cf98de727830c292e74f
Thanchanok T <6231302021@lamduan.mfu.ac.th>
2022-09-21 13:56:21 +0700

    lastest update

--- src/App2.js
+++ src/App2.js
@@ -1,5 +1,12 @@
 import * as React from "react";
 import "./App.css";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 function App2() {
   // const fruits = [
   //   { name: "banana", id: 100 },
@@ -54,21 +61,25 @@ function App2() {
 
   const addFruit = () => {
     //console.log(newFruit);
-    const newFruits =[...fruits];
-    newFruits.push({name: newFruit, picture:  "/images/banana.jpg", id: newFruits.length+101});
+    const newFruits = [...fruits];
+    newFruits.push({
+      name: newFruit,
+      picture: "/images/banana.jpg",
+      id: newFruits.length + 101,
+    });
     setFruits(newFruits);
   };
 
   return (
     <>
-      <button onClick={increase} disabled={counterStatus}>
+      {/* <button onClick={increase} disabled={counterStatus}>
         Click
       </button>
       <button onClick={reset}>Reset</button>
       <h1>Click me now :D</h1>
       <p>Counter = {counter}</p>
       <br />
-      <br />
+      <br /> */}
       <h1>Fruits</h1>
       <div className="btnAddLayout">
         <button className="btnAdd" onClick={() => setAddHiddle(!addHiddle)}>
@@ -87,14 +98,35 @@ function App2() {
       </div>
       {fruits.map((fruit) => {
         return (
-          <div className="fruit-item" key={fruit.id}>
-            <h2>{fruit.name}</h2>
-            <div className="avatar">
-              <img className="imgSize" src={fruit.picture} alt={fruit.name} />
-            </div>
-            <button onClick={() => deleteFruit(fruit.id)}>Delete</button>
-            <button onClick={editFruit}>Edit</button>
-          </div>
+          <Card sx={{ maxWidth: 345 , mb: 2}} >
+            <CardMedia
+              component="img"
+              height="140"
+              image={fruit.picture}
+              alt={fruit.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {fruit.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <button onClick={() => deleteFruit(fruit.id)}>Delete</button>
+              <button onClick={editFruit}>Edit</button>
+            </CardActions>
+          </Card>
+          // <div className="fruit-item" key={fruit.id}>
+          //   <h2>{fruit.name}</h2>
+          //   <div className="avatar">
+          //     <img className="imgSize" src={fruit.picture} alt={fruit.name} />
+          //   </div>
+          //   <button onClick={() => deleteFruit(fruit.id)}>Delete</button>
+          //   <button onClick={editFruit}>Edit</button>
+          // </div>
         );
       })}
     </>
